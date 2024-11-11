34157f1c0ea342dbab2c2baec133aaa74f0ef319
cdogcoder <cdogcoder@gmail.com>
2024-11-10 20:32:36 -0800

    Add paragraph to understand the problem

---
+++ script.js
@@ -0,0 +1,20 @@
+// Understand the problem:
+// The task is to create a functioning calculator.
+// The final product should have a usable user interface
+// and should be able to perform the following functions:
+// Do basic calculations between two operands (display the
+// current number clicked, the operator with a different 
+// background color upon button click, display the next
+// number clicked, and display the result of the calculation
+// in the output box upon 'equals' button click), perform 
+// calculations across results (ex. if the 'equals' button
+// is not pressed after one number, an operator, and then
+// another number is clicked and another operator is clicked
+// after, the result of the two operands and operator that was
+// clicked before should appear in the output box as the current 
+// number), clear the output box upon 'clear' button click, and
+// delete numbers off the output box upon 'delete' button click.
+// All of the number calculations should be done without the 
+// assistance of functions like eval() and all element changes
+// that occur on screen should be the product of event listeners
+// and DOM manipulation.
