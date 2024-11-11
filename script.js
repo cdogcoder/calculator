0e21f834574842b20be9b4a7dad135f7e7dcf3fc
cdogcoder <cdogcoder@gmail.com>
2024-11-10 21:03:04 -0800

    Add pseudocode/steps

--- script.js
+++ script.js
@@ -18,3 +18,51 @@
 // assistance of functions like eval() and all element changes
 // that occur on screen should be the product of event listeners
 // and DOM manipulation.
+
+// My pseudocode
+// 1. Write a function 'writeToOutput' that takes in an event
+//    type argument 'event' and uses it to write a value to the
+//    the output. The function should be called every time a number
+//    button is clicked and for operators, it should change the
+//    background color of the button to be darker, indicating
+//    that the operator has been selected for the calculation.
+//    The function itself should also be writing the different
+//    components of the calculation to some form of storage as it
+//    is happening (ex. if a user types '2 + 2', the first operand (2),
+//    the operator (+), and the second operand (2) should be recorded
+//    somewhere in the form of a string or array declared outside of the
+//    function's scope (or return the string/array after function call) so
+//    that it can be used in another function that will evaluate it).
+// 2. Write a function 'getEvaluatedExpression' that takes in a string/array
+//    type argument 'expression' and returns the value that comes from
+//    evaluating the expression. The function should be called every time the
+//    'equals' button is clicked or when a number button is clicked when two operands
+//    and an operator exist in the variable 'expression' (or whatever will record the
+//    values the user inputs). The function should only return the value
+//    and must not be written to the output box from the function call alone
+//    (if the function call to 'getEvaluatedExpression' happens as the 'equals'
+//    button is pressed, the function 'writeToOutput' should be written such that
+//    it displays the value; in other words, the function 'getEvaluatedExpression'
+//    will likely be called from within the function 'writeToOutput').
+// 3. Write a function 'clearOutput' that takes in no arguments and clears
+//    the output of any numbers or decimals, unselects/changes background color 
+//    of the buttons back to a uniform color, and resets the variable 'expression'
+//    (or whatever will record the values the user inputs). The function should be 
+//    called every time the 'Clear' button is clicked.
+// 4. Write a function 'deleteFromOutput' that takes in no arguments and deletes the
+//    last entered digit from the output, unselects/changes background color of the
+//    buttons back to a uniform color, and removes the last number that was appended to
+//    the variable 'expression' (or whatever will record the values that the user inputs).
+//    The function should be called every time the 'Delete' button is clicked. If the user
+//    clicks the 'Delete' button while still typing the first operand, the last entered digit
+//    will be removed from the output and the same from the variable 'expression'. If the user
+//    clicks the 'Delete' button after typing the first operand and clicking one of the operator
+//    buttons, the last entered digit will be removed from the output, the background color of the 
+//    operator button that was clicked will return to the uniform button color, and both the digit
+//    and the operator will be removed from the variable 'expression' (or whatever will record the
+//    values the user inputs). 
+// 5. Add keyboard support to the calculator. Every time a 'keypress' event
+//    is fired, check if the key was any of the ones that exist on the keyboard
+//    (Ctrl+C should be for the 'Clear' button and Ctrl+D for the 'Delete' button)
+//    and call the appropriate function for that key press as the program would for
+//    its corresponding button click.
